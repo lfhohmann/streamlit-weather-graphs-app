c54bf849a797770a72ec6fcaa7276b3acd5b1ebc
Lucas Hohmann <lfhohmann@gmail.com>
2021-07-08 13:56:36 -0300

    Removed test function

--- db_loader.py
+++ db_loader.py
@@ -1,11 +1,3 @@
 from boto3.dynamodb.conditions import Key, Attr
 import boto3
 import os
-
-
-def test(parameter):
-    return (
-        os.environ["AWS_ACCESS_KEY_ID"],
-        os.environ["AWS_SECRET_ACCESS_KEY"],
-        os.environ["AWS_SESSION_TOKEN"],
-    )
