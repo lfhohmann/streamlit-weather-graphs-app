18c2f4cfa73e460b97d0de9b6adbb5e38f6ecda0
Lucas Hohmann <lfhohmann@gmail.com>
2021-07-08 14:19:04 -0300

    Testing AWS config

--- db_loader.py
+++ db_loader.py
@@ -1,11 +1,19 @@
 from boto3.dynamodb.conditions import Key, Attr
+from botocore.config import Config
 import boto3
 
 DB_TABLE = "wunderground_pws"
 
 
+aws_config = Config(
+    region_name="us-east-1",
+    signature_version="v4",
+    retries={"max_attempts": 10, "mode": "standard"},
+)
+
+
 def db_load() -> list:
-    dynamodb = boto3.resource("dynamodb")
+    dynamodb = boto3.resource("dynamodb", config=aws_config)
     table = dynamodb.Table(DB_TABLE)
 
     response = table.scan()
