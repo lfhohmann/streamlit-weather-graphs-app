6f58ced2d0d8dd34c0987bb51404d9c2ffde7327
Lucas Hohmann <lfhohmann@gmail.com>
2021-07-08 13:40:49 -0300

    Init app file

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(
+    page_title="Weather Graphs",
+    page_icon="⛅",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
