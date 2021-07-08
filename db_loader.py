f0f2427e711b6721bc21742fdbde7b0b62f69a5d
Lucas Hohmann <lfhohmann@gmail.com>
2021-07-08 13:44:29 -0300

    Test importing from external files

--- db_loader.py
+++ db_loader.py
@@ -1,2 +1,6 @@
 from boto3.dynamodb.conditions import Key, Attr
 import boto3
+
+
+def test(parameter):
+    return parameter
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import plotly.express as px
 import streamlit as st
 
+import db_loader
+
 st.set_page_config(
     page_title="Weather Graphs",
     page_icon="⛅",
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+st.write(db_loader.test("db_loader test"))
