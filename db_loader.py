4be9b864ddc6d5b03822b574e10bfa0c5059ab43
Lucas Hohmann <lfhohmann@gmail.com>
2021-07-08 13:54:10 -0300

    Testing environment variables

--- db_loader.py
+++ db_loader.py
@@ -1,6 +1,11 @@
 from boto3.dynamodb.conditions import Key, Attr
 import boto3
+import os
 
 
 def test(parameter):
-    return parameter
+    return (
+        os.environ["AWS_ACCESS_KEY_ID"],
+        os.environ["AWS_SECRET_ACCESS_KEY"],
+        os.environ["AWS_SESSION_TOKEN"],
+    )
